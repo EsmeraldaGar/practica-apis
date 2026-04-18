dc5606b60e9c9ba15f3d8dea38f3cb8cf2e0f598
Esmeralda Martinez Garcia <emagarci@emeal.nttdata.com>
2026-04-17 21:10:12 -0600

    ref: endpoint GET /clima con OpenWeatherMap, manejo de errores

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 import httpx
 import os
 from dotenv import load_dotenv
@@ -19,16 +19,29 @@ async def obtener_clima(ciudad: str = Query(..., description= "Nombre de la ciud
     # la construccion de la url weathermap
     url = "http://api.openweathermap.org/data/2.5/weather"
 
-    #?q=London,uk&APPID=a7064514ee7eb431bc63683bfca93ddb
+    # ?q=London,uk&APPID=a7064514ee7eb431bc63683bfca93ddb
     params = {
         "q": ciudad,
         "appid": API_KEY,
+        "units": "metric", # temperatura en cels
         "lang": "es" # descripciones en español
     }
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, params=params)
-        data = response.json()
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=params)
+
+            response.raise_for_status() # http es codigo 404
+            data = response.json()
+
+                        # captura error y traducir a errores Bad Request
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=400, detail=f"Error de OpenWeatherMap: {e.response.text}")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error del servidor: {str(e)}")
+
 
     return data
 
